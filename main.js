0f972cec7802d072fcacf5b04448bbbb3f3ca596
harryhs99 <harry.hstaples@hotmail.com>
2024-04-10 09:39:34 +0100

    complete under construction version 1

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// consoleText(["Under Construction", "Back Soon..."], "text", ["#03A062"]);
-
 writeText(["Under Construction", "Back Soon..."], "statement", "#008529");
 
 function writeText(words, id, colour) {
@@ -11,16 +9,35 @@ function writeText(words, id, colour) {
 
   // variables
   let caretVisible = true;
-  let letterCount = 1;
-  let index = 1;
+  let letterCount = 0;
+  let n = 1;
   let waiting = false;
 
   target.setAttribute("style", "color:" + colour);
 
   // write the text
   window.setInterval(function () {
-    target.innerHTML = words[0].substring(0, letterCount);
-    letterCount += index;
+    if (letterCount === -1 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount);
+      window.setTimeout(function () {
+        let usedWord = words.shift();
+        words.push(usedWord);
+        n = 1;
+        letterCount += n;
+        waiting = false;
+      }, 1000);
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(function () {
+        n = -1;
+        letterCount += n;
+        waiting = false;
+      }, 1000);
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount);
+      letterCount += n;
+    }
   }, 135);
 
   // blink the caret
@@ -33,51 +50,4 @@ function writeText(words, id, colour) {
       caretVisible = true;
     }
   }, 450);
-}
-
-function consoleText(words, id, colors) {
-  if (colors === undefined) colors = ["#fff"];
-  let visible = true;
-  const con = document.getElementById("console");
-  let letterCount = 1;
-  let x = 1;
-  let waiting = false;
-  const target = document.getElementById(id);
-  target.setAttribute("style", "color:" + colors[0]);
-  window.setInterval(function () {
-    if (letterCount === 0 && waiting === false) {
-      waiting = true;
-      target.innerHTML = words[0].substring(0, letterCount);
-      window.setTimeout(function () {
-        let usedColor = colors.shift();
-        colors.push(usedColor);
-        let usedWord = words.shift();
-        words.push(usedWord);
-        x = 1;
-        target.setAttribute("style", "color:" + colors[0]);
-        letterCount += x;
-        waiting = false;
-      }, 1000);
-    } else if (letterCount === words[0].length + 1 && waiting === false) {
-      waiting = true;
-      window.setTimeout(function () {
-        x = -1;
-        letterCount += x;
-        waiting = false;
-      }, 1000);
-    } else if (waiting === false) {
-      target.innerHTML = words[0].substring(0, letterCount);
-      letterCount += x;
-    }
-  }, 120);
-  window.setInterval(function () {
-    if (visible === true) {
-      con.className = "console-underscore hidden";
-      visible = false;
-    } else {
-      con.className = "console-underscore";
-
-      visible = true;
-    }
-  }, 400);
 }
